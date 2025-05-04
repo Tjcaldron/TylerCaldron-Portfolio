8ab3df10d8768f0077129d3925ab8ac902a74cd1
Tjcaldron <46572559+Tjcaldron@users.noreply.github.com>
2025-05-03 22:51:34 -0500

    Added README and updated covid_ds.py data

--- covid data/covid_ds.py
+++ covid data/covid_ds.py
@@ -5,6 +5,19 @@ import matplotlib.pyplot as plot
 
 file_path = 'archive/usa_county_wise.csv'
 df = pd.read_csv(file_path)
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values(by='Date')
 
-df.plot(x="Date", y="Confirmed", kind="line", title="US Confirmed COVID-19 Cases")
+daily_totals = df.groupby("Date")[['Confirmed', 'Deaths']].sum().reset_index()
+
+df.plot(x="Date", 
+y=["Confirmed", "Deaths"], 
+kind="line", 
+title="US Confirmed COVID-19 Cases Vs. Deaths")
+
+plot.xlabel("Date")
+plot.ylabel("Number of Cases")
+plot.grid(True)
+plot.legend()
+plot.tight_layout()
 plot.show()
